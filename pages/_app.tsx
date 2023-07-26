6c2dbbfa6ad83f760bcaa074b2f01d0faf1eddd1
Niels <niels.vder.vlist@gmail.com>
2023-07-26 22:27:13 +0200

    Add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react';
 import '../styles/index.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
@@ -7,7 +8,10 @@ import '../styles/global.scss'
 config.autoAddCss = false
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Component {...pageProps} />
+    <Analytics />
+  </>
 }
 
 export default MyApp
