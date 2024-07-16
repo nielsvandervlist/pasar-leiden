ae8e7fc188a27ec04d726d660de6f409f95e932c
Niels van der Vlist <nielsvandervlist@leiderdorpinstruments.nl>
2024-07-16 15:54:14 +0200

    Add QR link to qr check page

--- lib/generateQRCode.ts
+++ lib/generateQRCode.ts
@@ -4,8 +4,10 @@ import CryptoJS from "crypto-js";
 export const generateQRCode = async (text: string): Promise<string> => {
     const signature = CryptoJS.HmacSHA256(text, process.env.SECRET_KEY).toString();
     const dataWithSignature = `${text}|${signature}`;
+    const url = `${process.env.LOCAL_URL}/qr-check?paymentId=${encodeURIComponent(dataWithSignature)}`;
+
     try {
-        return await QRCode.toDataURL(dataWithSignature);
+        return await QRCode.toDataURL(url);
     } catch (err) {
         console.error('Failed to generate QR code:', err);
         throw err;
---
+++ pages/qr-check/index.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import Head from "next/head";
+import Container from "../../components/container"; // Adjust the import path based on your file structure
+import Layout from "../../components/layout"; // Adjust the import path based on your file structure
+
+export default function Qrcheck() {
+    const router = useRouter();
+    const [loading, setLoading] = useState(true);
+    const [paymentStatus, setPaymentStatus] = useState<string | null>(null);
+
+    useEffect(() => {
+        // Extract the paymentId from the query parameters
+        const { paymentId } = router.query;
+
+        if (paymentId) {
+            // Fetch the payment status from the backend
+            const fetchPaymentStatus = async () => {
+                try {
+                    const response = await fetch(`/api/qr-status?paymentId=${encodeURIComponent(paymentId as string)}`);
+                    const data = await response.json();
+                    setPaymentStatus(data.status);
+                } catch (error) {
+                    console.error('Error fetching payment status:', error);
+                    setPaymentStatus('Error fetching payment status');
+                } finally {
+                    setLoading(false);
+                }
+            };
+
+            fetchPaymentStatus().then(r => console.log(r));
+        } else {
+            setLoading(false);
+        }
+    }, [router.query]);
+
+    if (loading) {
+        return <p>Loading...</p>;
+    }
+
+    return (
+        <Layout preview={''}>
+            <Head>
+                <title>{`Check QR`}</title>
+            </Head>
+            <Container>
+                <div>
+                    <h1>QR CHECK</h1>
+                    {paymentStatus === 'paid' ? (
+                        <p>Ticket is paid and key is valid</p>
+                    ) : (
+                        <p>There was an issue with your payment or key is not valid. Please try again.</p>
+                    )}
+                </div>
+            </Container>
+        </Layout>
+    );
+}
