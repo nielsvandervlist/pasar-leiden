fc0220c3e649596532ceb399214e9469ec57e33b
Niels van der Vlist <nielsvandervlist@leiderdorpinstruments.nl>
2024-07-16 15:26:55 +0200

    Add crypto for hashing QR

--- lib/generateQRCode.ts
+++ lib/generateQRCode.ts
@@ -1,8 +1,11 @@
 import QRCode from 'qrcode';
+import CryptoJS from "crypto-js";
 
 export const generateQRCode = async (text: string): Promise<string> => {
+    const signature = CryptoJS.HmacSHA256(text, process.env.SECRET_KEY).toString();
+    const dataWithSignature = `${text}|${signature}`;
     try {
-        return await QRCode.toDataURL(text);
+        return await QRCode.toDataURL(dataWithSignature);
     } catch (err) {
         console.error('Failed to generate QR code:', err);
         throw err;
