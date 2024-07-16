88fa36d549453c86ba9bebef6b2c0439426959dd
Niels van der Vlist <nielsvandervlist@leiderdorpinstruments.nl>
2024-07-16 09:36:41 +0200

    Check if api key is stored

--- lib/mollie.ts
+++ lib/mollie.ts
@@ -1,5 +1,11 @@
-import { createMollieClient } from '@mollie/api-client';
+import { createMollieClient, MollieClient } from '@mollie/api-client';
 
-const mollieClient = createMollieClient({ apiKey: process.env.MOLLIE_API_KEY });
+const apiKey = process.env.MOLLIE_API_KEY;
+
+if (!apiKey) {
+    throw new Error('Missing Mollie API key');
+}
+
+const mollieClient: MollieClient = createMollieClient({ apiKey });
 
 export default mollieClient;
