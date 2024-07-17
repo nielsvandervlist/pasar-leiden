73807cd0e6fce7188ee2be116e8bd24e0089464f
Niels van der Vlist <nielsvandervlist@leiderdorpinstruments.nl>
2024-07-17 11:33:40 +0200

    Edit qrcode check

--- pages/qr-check/index.tsx
+++ pages/qr-check/index.tsx
@@ -2,12 +2,14 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import Head from "next/head";
 import Container from "../../components/container"; // Adjust the import path based on your file structure
-import Layout from "../../components/layout"; // Adjust the import path based on your file structure
+import Layout from "../../components/layout";
+import mollieClient from "../../lib/mollie"; // Adjust the import path based on your file structure
 
 export default function Qrcheck() {
     const router = useRouter();
     const [loading, setLoading] = useState(true);
     const [paymentStatus, setPaymentStatus] = useState<string | null>(null);
+    const [scanned, setScanned] = useState<boolean | null>(null);
 
     useEffect(() => {
         // Extract the paymentId from the query parameters
@@ -20,6 +22,16 @@ export default function Qrcheck() {
                     const response = await fetch(`/api/qr-status?paymentId=${encodeURIComponent(paymentId as string)}`);
                     const data = await response.json();
                     setPaymentStatus(data.status);
+                    if(!data.metadata.scanned) {
+                        await mollieClient.payments.update(paymentId as string, {
+                            metadata: {
+                                email: data.email,
+                                scanned: true,
+                            }
+                        })
+                    } else {
+                        setScanned(true)
+                    }
                 } catch (error) {
                     console.error('Error fetching payment status:', error);
                     setPaymentStatus('Error fetching payment status');
@@ -46,7 +58,7 @@ export default function Qrcheck() {
             <Container>
                 <div>
                     <h1>Thank You!</h1>
-                    {paymentStatus === 'paid' ? (
+                    {paymentStatus === 'paid' && !scanned ? (
                         <p>Ticket is paid and key is valid</p>
                     ) : (
                         <p>There was an issue with your payment or key is not valid. Please try again.</p>
